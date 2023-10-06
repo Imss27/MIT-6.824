31014e5113f5cb7d993c0d4cd4db7526f103d97c
Imss27 <1529767689@qq.com>
2023-10-07 03:19:44 +0800

    lab1

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,12 +24,26 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type WorkerArgs struct {
+	MapTaskNumber    int // finished maptask number
+	ReduceTaskNumber int // finished reducetask number
+}
 
+type WorkerReply struct {
+	Tasktype int // 0: map task, 1: reduce task, 2: waiting, 3: job finished
+	NMap     int // number of map task
+	NReduce  int // number of reduce task
+
+	MapTaskNumber int    // map task only
+	Filename      string // maptask only
+
+	ReduceTaskNumber int // reducetask only
+}
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
 // Athena AFS doesn't support UNIX-domain sockets.
-func coordinatorSock() string {
+func masterSock() string {
 	s := "/var/tmp/5840-mr-"
 	s += strconv.Itoa(os.Getuid())
 	return s
